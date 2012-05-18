94e6dd0452b16697ad10f9bc7e0e7431c95d9020
Milktrader <milktrader@gmail.com>
2012-05-18 16:42:16 -0400

    added deparse substitute to plotEquityCurve function

--- xts/plotEquityCurve.r
+++ xts/plotEquityCurve.r
@@ -1,12 +1,13 @@
  plotEquityCurve <- function(x) { 
    
+   nam  = deparse(substitute(x)) 
    x$eq = NA
    
    for( i in 1:nrow(x)){
        
        x$eq[i,] =  apply(x[,1][1:i], 2, Return.cumulative)}
 
-   plot(x$eq + 1)
+   plot(x$eq + 1, main=nam, las=1, xaxt='n')
 
  }
    
