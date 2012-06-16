e0cf3850a69d8c76c03a07019e15a2a7a5ce8e2d
Milktrader <milktrader@gmail.com>
2012-06-16 10:13:41 -0400

    added a chart_theme to bee.r

--- quantstrat/bee.r
+++ quantstrat/bee.r
@@ -11,11 +11,15 @@ suppressWarnings(rm("'bug'","'bug'","'GSPC'","bee","initDate","initEq",'start_t'
 currency('USD')
 stock('GSPC',currency='USD',multiplier=1)
 
-initDate='2000-12-31'
-foo = as.Date(initDate)
-initEq=1000000
-sd = .5
+initDate='2011-12-31'
 
+foo = as.Date(initDate)
+million = 1e6
+initEq= million
+
+SD = .5
+SLOW = 30
+FAST = 10
 
 initPortf('bug',symbols='GSPC', initDate=initDate)
 initAcct('bug',portfolios='bug', initDate=initDate)
@@ -25,7 +29,7 @@ addPosLimit(
             portfolio='bug',
             symbol='GSPC', 
             timestamp = as.character(foo - 1),  
-            maxpos=100)
+            maxpos=200)
 
 
 bee <- strategy('bug')
@@ -34,32 +38,32 @@ bee <- add.indicator(
                               strategy = bee, 
                               name = "BBands", 
                               arguments = list(HLC=quote(HLC(mktdata)), 
-                                               n=30, 
-                                               sd=sd))
+                                               n=SLOW, 
+                                               sd=SD))
 
 bee <- add.indicator(
                               strategy = bee, 
                               name = "SMA", 
                               arguments = list(x=quote(Cl(mktdata)), 
-                                               n=10),
-                              label= "ma10" )
+                                               n=FAST),
+                              label= "fast" )
 bee <- add.signal(
                            strategy = bee,
                            name="sigCrossover",
-                           arguments = list( columns=c('ma10',"dn"), 
+                           arguments = list( columns=c('fast',"dn"), 
                                             relationship="lt"),
-                           label="ma10.lt.dn")
+                           label="fast.lt.dn")
 bee <- add.signal(
                            strategy = bee,
                            name="sigCrossover",
-                           arguments = list( columns=c("ma10","dn"),
+                           arguments = list( columns=c("fast","dn"),
                                             relationship="gt"),
-                           label="ma10.gt.up")
+                           label="fast.gt.up")
 
 bee <- add.rule(
                          strategy = bee,
                          name='ruleSignal', 
-                         arguments = list(sigcol="ma10.gt.up",
+                         arguments = list(sigcol="fast.gt.up",
                                           sigval=TRUE, 
                                           orderqty=100, 
                                           ordertype='market', 
@@ -70,18 +74,7 @@ bee <- add.rule(
 bee <- add.rule(
                          strategy = bee,
                          name='ruleSignal', 
-                         arguments = list(sigcol="ma10.lt.dn",
-                                          sigval=TRUE, 
-                                          orderqty='all', 
-                                          ordertype='market', 
-                                          orderside='long'),
-                         type='exit')
-
-
-bee <- add.rule(
-                         strategy = bee,
-                         name='ruleSignal', 
-                         arguments = list(sigcol="ma10.lt.dn",
+                         arguments = list(sigcol="fast.lt.dn",
                                           sigval=TRUE, 
                                           orderqty=-100, 
                                           ordertype='market', 
@@ -91,7 +84,17 @@ bee <- add.rule(
 bee <- add.rule(
                          strategy = bee,
                          name='ruleSignal', 
-                         arguments = list(sigcol="ma10.gt.up",
+                         arguments = list(sigcol="fast.lt.dn",
+                                          sigval=TRUE, 
+                                          orderqty='all', 
+                                          ordertype='market', 
+                                          orderside='long'),
+                         type='exit')
+
+bee <- add.rule(
+                         strategy = bee,
+                         name='ruleSignal', 
+                         arguments = list(sigcol="fast.gt.up",
                                           sigval=TRUE, 
                                           orderqty='all', 
                                           ordertype='market', 
@@ -113,5 +116,27 @@ end_t<-Sys.time()
 print("trade blotter portfolio update:")
 print(end_t-start_t)
 
-chart.Posn(Portfolio='bug',Symbol='GSPC')
+
+chart_theme <- mytheme <- function() {
+  theme <-list(col=list(bg='ivory',
+                        label.bg='ivory',
+                        grid='ivory',
+                        grid2='ivory',
+                        ticks=1,
+                        labels=4,
+                        line.col='ivory',
+                        dn.col='lightcyan',
+                        up.col='lightcyan',
+                        dn.border='lightblue',
+                        up.border='lightblue'),
+               shading=1,
+               format.labels=TRUE,
+               coarse.time=FALSE,
+               rylab=FALSE,
+               lylab=TRUE,
+               grid.ticks.lwd=1)
+  theme
+}
+
+chart.Posn(Portfolio='bug',Symbol='GSPC', theme=chart_theme(), clev=0)
 
