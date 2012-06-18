1a20718b4c88ab8f6cddb4a159e09e163bd65804
Milktrader <milktrader@gmail.com>
2012-06-17 23:54:04 -0400

    merged long and short port into one script

--- quantstrat/open3.r
+++ quantstrat/open3.r
@@ -1,5 +1,3 @@
-
-
 #!/usr/bin/Rscript --no-save
 
 initDate = '2002-10-21'
@@ -10,7 +8,8 @@ initEq = 100000
 
 ####
 
-p = 'bug'
+green = 'green'
+red   = 'red'
 a = 'IB1'
 
 ###
@@ -20,60 +19,35 @@ getSymbols('AAPL', from=.from, to=.to, verbose=FALSE)
 
 ###
 
-initPortf(p, symbols='AAPL', initDate=initDate, currency='USD')
-initAcct(a, portfolios=p, initDate=initDate, currency='USD')
+initPortf(green, symbols='AAPL', initDate=initDate, currency='USD')
+initPortf(red  , symbols='AAPL', initDate=initDate, currency='USD')
+initAcct(a, portfolios=c(green, red), initDate=initDate, currency='USD')
 
 ###
 
-initOrders(p, initDate=initDate)
-bee <- strategy(p)
+initOrders(green, initDate=initDate)
+initOrders(red  , initDate=initDate)
+green.bee <- strategy(green)
+red.bee   <- strategy(red)
 
 # indicators
 
-bee <- add.indicator(bee, name="SMA", arguments = list(x = quote(Cl(mktdata)), n=10), label="SmaFAST")
-bee <- add.indicator(bee, name="SMA", arguments = list(x = quote(Cl(mktdata)), n=30), label="SmaSLOW")
+green.bee <- add.indicator(green.bee, name="SMA", arguments = list(x = quote(Cl(mktdata)), n=10), label="SmaFAST")
+green.bee <- add.indicator(green.bee, name="SMA", arguments = list(x = quote(Cl(mktdata)), n=30), label="SmaSLOW")
+red.bee   <- add.indicator(red.bee, name="SMA", arguments = list(x = quote(Cl(mktdata)), n=10), label="SmaFAST")
+red.bee   <- add.indicator(red.bee, name="SMA", arguments = list(x = quote(Cl(mktdata)), n=30), label="SmaSLOW")
 
 # signals
 
-bee <- add.signal(bee, 'sigCrossover', arguments = list(columns=c("SmaFAST","SmaSLOW"), relationship="gte"), label='fast.gt.up')
-bee <- add.signal(bee, 'sigCrossover', arguments = list(columns=c("SmaFAST","SmaSLOW"), relationship="lt"), label='fast.lt.dn')
+green.bee <- add.signal(green.bee, 'sigCrossover', arguments = list(columns=c("SmaFAST","SmaSLOW"), relationship="gte"), label='fast.gt.up')
+green.bee <- add.signal(green.bee, 'sigCrossover', arguments = list(columns=c("SmaFAST","SmaSLOW"), relationship="lt"), label='fast.lt.dn')
 
+red.bee <- add.signal(red.bee, 'sigCrossover', arguments = list(columns=c("SmaFAST","SmaSLOW"), relationship="gte"), label='fast.gt.up')
+red.bee <- add.signal(red.bee, 'sigCrossover', arguments = list(columns=c("SmaFAST","SmaSLOW"), relationship="lt"), label='fast.lt.dn')
 # rules
 
-bee <- add.rule(
-                         strategy = bee,
-                         name='ruleSignal',
-                         arguments = list(sigcol="fast.lt.dn",
-                                          sigval=TRUE,
-                                          orderqty='all',
-                                          ordertype='market',
-                                          orderside='long'),
-                         type='exit',
-                         label='ExitLONG')
-
-bee <- add.rule(
-                         strategy = bee,
-                         name='ruleSignal',
-                         arguments = list(sigcol="fast.gt.up",
-                                          sigval=TRUE,
-                                          orderqty='all',
-                                          ordertype='market',
-                                          orderside='short'),
-                         type='exit',
-                         label='ExitSHORT')
-bee <- add.rule(
-                         strategy = bee,
-                         name='ruleSignal',
-                         arguments = list(sigcol="fast.lt.dn",
-                                          sigval=TRUE,
-                                          orderqty=-100,
-                                          ordertype='market',
-                                          orderside='short'),
-                         type='enter',
-                         label='EnterSHORT')
-
-bee <- add.rule(
-                         strategy = bee,
+green.bee <- add.rule(
+                         strategy = green.bee,
                          name='ruleSignal',
                          arguments = list(sigcol="fast.gt.up",
                                           sigval=TRUE,
@@ -83,13 +57,54 @@ bee <- add.rule(
 
                          type='enter',
                          label='EnterLONG')
+green.bee <- add.rule(
+                         strategy = green.bee,
+                         name='ruleSignal',
+                         arguments = list(sigcol="fast.lt.dn",
+                                          sigval=TRUE,
+                                          orderqty='all',
+                                          ordertype='market',
+                                          orderside='long'),
+                         type='exit',
+                         label='ExitLONG')
+
+red.bee <- add.rule(
+                         strategy = red.bee,
+                         name='ruleSignal',
+                         arguments = list(sigcol="fast.lt.dn",
+                                          sigval=TRUE,
+                                          orderqty=-100,
+                                          ordertype='market',
+                                          orderside='short'),
+                         type='enter',
+                         label='EnterSHORT')
+red.bee  <- add.rule(
+                         strategy = red.bee,
+                         name='ruleSignal',
+                         arguments = list(sigcol="fast.gt.up",
+                                          sigval=TRUE,
+                                          orderqty='all',
+                                          ordertype='market',
+                                          orderside='short'),
+                         type='exit',
+                         label='ExitSHORT')
+
 #
 
-applyStrategy(bee, p, prefer='Open', verbose = FALSE)
+applyStrategy(green.bee, green, prefer='Open', verbose = FALSE)
+applyStrategy(red.bee, red, prefer='Open', verbose = FALSE)
 
-print(getOrderBook(p))
+print(getOrderBook(green))
+print(getOrderBook(red))
 
-txns <- getTxns(p, 'AAPL')
-txns
-cat('Net profit:', sum(txns$Net.Txn.Realized.PL), '\n')
+green.txns <- getTxns(green, 'AAPL')
+red.txns <- getTxns(red, 'AAPL')
+
+green.txns
+red.txns
+
+
+cat('Net profit from long side:', sum(green.txns$Net.Txn.Realized.PL), '\n')
+cat('Net profit from short side:', sum(red.txns$Net.Txn.Realized.PL), '\n')
+
 
