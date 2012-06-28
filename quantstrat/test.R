ccb99b8406802cb0f99c2b1b6e9b7723b743ac34
Milktrader <milktrader@gmail.com>
2012-06-28 12:21:58 -0400

    fixed t1,t2,t3 to hold numeric value

--- quantstrat/test.R
+++ quantstrat/test.R
@@ -2,9 +2,9 @@
 bookStatus = .strategy$order_book.bug$bug$GSPC$Order.Status 
 bookQty    = .strategy$order_book.bug$bug$GSPC$Order.Qty
 
-t1 = bookStatus[bookStatus$Order.Status == 'rejected']    # 34
-t2 = bookStatus[bookStatus$Order.Status == 'closed']      # 51
-t3 = bookStatus[bookStatus$Order.Status == 'open']        # 2 
+t1 = nrow(bookStatus[bookStatus$Order.Status == 'rejected'])    # 34
+t2 = nrow(bookStatus[bookStatus$Order.Status == 'closed'])      # 51
+t3 = nrow(bookStatus[bookStatus$Order.Status == 'open'])        # 2 
 
 t4 = nrow(mktdata[mktdata$fast.gt.up == 1])   # 61
 t5 = nrow(mktdata[mktdata$fast.lt.dn == 1])   # 54 
