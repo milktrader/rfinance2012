92444cb262253679d72aaadbf5100fab742e1fa3
Milktrader <milktrader@gmail.com>
2012-06-29 00:50:29 -0400

    incorporated test script into drone

--- quantstrat/drone.r
+++ quantstrat/drone.r
@@ -132,36 +132,57 @@ updateAcct(acct)
 
 returns = PortfReturns(acct)
 
-######################### TEST STATISTICS ####################
+######################### svUnit TESTS ####################
 
-#bookStatus = .strategy$order_book.bug$bug$GSPC$Order.Status 
-#bookQty    = .strategy$order_book.bug$bug$GSPC$Order.Qty
-#
-#t1 = nrow(bookStatus[bookStatus$Order.Status == 'rejected'])    # 34
-#t2 = nrow(bookStatus[bookStatus$Order.Status == 'closed'])      # 162
-#t3 = nrow(bookStatus[bookStatus$Order.Status == 'open'])        # 2 
-#
-#t4 = nrow(mktdata[mktdata$fast.gt.up == 1])   # 61
-#t5 = nrow(mktdata[mktdata$fast.lt.dn == 1])   # 54 
-#
-#t6 = suppressWarnings(max(as.numeric(bookQty), na.rm=TRUE)) # 100
-#t7 = suppressWarnings(min(as.numeric(bookQty), na.rm=TRUE)) # -100
-#
-#print('Test Results:')
-#print('')
-#
-#myUnit  <- function(t1, t2, t3, t4, t5, t6, t7) {
-#
-#    ifelse(t1 == 34,   print("PASS"), print("FAIL - rejected total wrong"))
-#    ifelse(t2 == 162,  print("PASS"), print("FAIL - closed total wrong"))
-#    ifelse(t3 == 2,    print("PASS"), print("FAIL - open total wrong"))
-#    ifelse(t4 == 61,   print("PASS"), print("FAIL - long signals wrong"))
-#    ifelse(t5 == 54,   print("PASS"), print("FAIL - short signals wrong"))
-#    ifelse(t6 == 100,  print("PASS"), print("FAIL - max position wrong"))
-#    ifelse(t7 == -100, print("PASS"), print("FAIL - min position wrong"))
-#}
-#
-#myUnit(t1,t2,t3,t4,t5,t6,t7)
-#
+suppressMessages(require(svUnit))
 
+######### DEFINE OUTSIDE TEST FUNCTION ############
 
+statusCheck = 'should have correct number of each status'
+signalCheck = 'should have the correct number of signals'
+qtyCheck    = 'should not exceed 100'
+
+############ DEFINE INSIDE TEST FUNCTION ################
+
+test(statusCheck) = function() {
+
+  Status = .strategy$order_book.bug$bug$GSPC$Order.Status 
+
+  t1     = nrow(Status[Status$Order.Status == 'rejected'])    # 34
+  t2     = nrow(Status[Status$Order.Status == 'closed'])      # 162
+  t3     = nrow(Status[Status$Order.Status == 'open'])        # 2 
+
+  checkEquals(t1, 34)
+  checkEquals(t2, 162)
+  checkEquals(t3, 2)
+
+}
+
+test(signalCheck) = function() {
+    
+  t4 = nrow(mktdata[mktdata$fast.gt.up == 1])   # 61
+  t5 = nrow(mktdata[mktdata$fast.lt.dn == 1])   # 54 
+  
+  checkEquals(t4, 61)
+  checkEquals(t5, 54)
+}
+
+test(qtyCheck) = function() {
+    
+  Qty    = .strategy$order_book.bug$bug$GSPC$Order.Qty
+
+  t6     = suppressWarnings(max(as.numeric(Qty), na.rm=TRUE)) # 100
+  t7     = suppressWarnings(min(as.numeric(Qty), na.rm=TRUE)) # -100
+  
+  checkEquals(t6, 100)
+  checkEquals(t7, -100)
+}
+ 
+################ RUN THE TESTS ###############################
+
+clearLog()
+runTest(statusCheck)
+runTest(signalCheck)
+runTest(qtyCheck)
+Log()
+summary(Log())
